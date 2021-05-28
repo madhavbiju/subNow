c189e3399a0b6a0e74c5760db8d692c506d24bed
Madhav Biju <65170463+madhavbiju@users.noreply.github.com>
2021-05-28 11:53:38 +0530

    Switched to Bootstrap CSS

---
+++ script.js
@@ -0,0 +1,166 @@
+      document.write("<p>");
+     function addZero(i) {
+      if (i < 10) {
+      i = "0" + i;
+      }
+      return i;
+     }
+    var d = new Date();
+    var h = addZero(d.getHours());
+    var m = addZero(d.getMinutes());
+    var time= h + "" + m;
+    var date = d.getDay();
+    if (date == 1)
+    {
+     if ((time>=0850)&&(time<0950)) 
+        {
+             document.write('<h1>Operating Systems</h1><h2>9:00-9:50</h2><a href="https://bit.ly/3toCNck" target="_blank" rel="noopener noreferrer">Join via Teams</a>');
+        }
+        if ((time>=0950)&&(time<1050)) 
+        {
+          document.write('<h1>Database Management System</h1><h2>10:00-10:50</h2><a href="https://bit.ly/3nT4lpl" target="_blank" rel="noopener noreferrer">Join via Teams</a>');
+        }
+        if ((time>=1050)&&(time<1150)) 
+        {
+          document.write('<h1>COA</h1><h2>11:00-11:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        if ((time>=1150)&&(time<1250)) 
+        {
+          document.write('<h1>Database Management System</h1><h2>12:00-12:50</h2><a href="https://bit.ly/3nT4lpl" target="_blank" rel="noopener noreferrer">Join via Teams</a>');
+        }
+        if ((time>=1350)&&(time<1450)) 
+        {
+          document.write('<h1>Graph Theory</h1><h2>02:00-02:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        if ((time>=1450)&&(time<1550)) 
+        {
+          document.write('<h1>Operating System Lab</h1><h2>03:00-03:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        document.write("</p>");
+    }
+    
+    if (date == 2)
+    {
+     if ((time>=0850)&&(time<0950)) 
+        {
+             document.write('<h1>Database Management System</h1><h2>9:00-9:50</h2><a href="https://bit.ly/3nT4lpl" target="_blank" rel="noopener noreferrer">Join via Teams</a>');
+        }
+        if ((time>=0950)&&(time<1050)) 
+        {
+          document.write('<h1>Operating Systems</h1><h2>10:00-10:50</h2><a href="https://bit.ly/3toCNck" target="_blank" rel="noopener noreferrer">Join via Teams</a>');
+        }
+        if ((time>=1050)&&(time<1150)) 
+        {
+          document.write('<h1>Graph Theory</h1><h2>11:00-11:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        if ((time>=1150)&&(time<1250)) 
+        {
+          document.write('<h1>Minor</h1><h2>12:00-12:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        if ((time>=1350)&&(time<1450)) 
+        {
+          document.write('<h1>Coding Class</h1><h2>02:00-02:50</h2><a href=#>Link in Mail</a>');
+        }
+        if ((time>=1450)&&(time<1550)) 
+        {
+          document.write('<h1>Honours</h1><h2>03:00-03:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        document.write("</p>");
+    }
+    
+    if (date == 3)
+    {
+     if ((time>=0850)&&(time<0950)) 
+        {
+             document.write('<h1>Minor</h1><h2>9:00-9:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        if ((time>=0950)&&(time<1050)) 
+        {
+          document.write('<h1>Database Management System</h1><h2>10:00-10:50</h2><a href="https://bit.ly/3nT4lpl" target="_blank" rel="noopener noreferrer">Join via Teams</a>');
+        }
+        if ((time>=1050)&&(time<1150)) 
+        {
+          document.write('<h1>Graph Theory</h1><h2>11:00-11:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        if ((time>=1150)&&(time<1250)) 
+        {
+          document.write('<h1>Operating Systems</h1><h2>12:00-12:50</h2><a href="https://bit.ly/3toCNck" target="_blank" rel="noopener noreferrer">Join via Teams</a>');
+        }
+        if ((time>=1350)&&(time<1450)) 
+        {
+          document.write('<h1>COA</h1><h2>02:00-02:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        if ((time>=1450)&&(time<1550)) 
+        {
+          document.write('<h1>Honours</h1><h2>03:00-03:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        document.write("</p>");
+    }
+
+    if (date == 4)
+    {
+        if ((time>=0850)&&(time<0950)) 
+        {
+             document.write('<h1>Minor</h1><h2>9:00-9:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        if ((time>=0950)&&(time<1050)) 
+        {
+          document.write('<h1>Constitution of India</h1><h2>10:00-10:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        if ((time>=1050)&&(time<1150)) 
+        {
+          document.write('<h1>COA</h1><h2>11:00-11:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        if ((time>=1150)&&(time<1250)) 
+        {
+          document.write('<h1>Graph Theory</h1><h2>12:00-12:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        if ((time>=1350)&&(time<1450)) 
+        {
+          document.write('<h1>Graph Theory</h1><h2>02:00-02:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        if ((time>=1450)&&(time<1550)) 
+        {
+          document.write('<h1>Honours</h1><h2>03:00-03:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        document.write("</p>");
+    }
+
+    if (date == 5)
+    {
+     if ((time>=0850)&&(time<0950)) 
+        {
+             document.write('<h1>Database Management System</h1><h2>9:00-9:50</h2><a href="https://bit.ly/3nT4lpl" target="_blank" rel="noopener noreferrer">Join via Teams</a>');
+        }
+        if ((time>=0950)&&(time<1050)) 
+        {
+          document.write('<h1>COA</h1><h2>10:00-10:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        if ((time>=1050)&&(time<1150)) 
+        {
+          document.write('<h1>Operating Systems</h1><h2>11:00-11:50</h2><a href="https://bit.ly/3toCNck" target="_blank" rel="noopener noreferrer">Join via Teams</a>');
+        }
+        if ((time>=1150)&&(time<1250)) 
+        {
+          document.write('<h1>Minor</h1><h2>12:00-12:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        if ((time>=1350)&&(time<1450)) 
+        {
+          document.write('<h1>Design and Engineering</h1><h2>02:00-02:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        if ((time>=1450)&&(time<1550)) 
+        {
+          document.write('<h1>Honours</h1><h2>03:00-03:50</h2><a href="https://bit.ly/3tkIY1b" target="_blank" rel="noopener noreferrer">Join via Linways</a>');
+        }
+        document.write("</p>");
+    }
+    
+    if((time>=1250)&&(time<1350)&&(date!=0)&&(date!=6))
+    {
+      document.write('<h1>Lunch Break</h1>')
+    }
+    
+    if((time<0850)||(time>1550)||(date==0)||(date==6))
+    {
+        document.write('<h1>No Class at this time</h1>');
+    }
